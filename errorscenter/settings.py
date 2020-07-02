62d855d85cc2c4da166b702887bffb8a15b26965
Johannes Ferreira <johannesssf@gmail.com>
2020-07-02 11:26:13 -0300

    Move django secret key to an environment variable

--- errorscenter/settings.py
+++ errorscenter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '05(h53=)$hx-lsz3@73#k32^cyf&n^qimof!(qag&z=6uy38*6'
+SECRET_KEY = os.getenv('ACELERADEV_FINAL_PROJ_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
