60177e3b46d3fa5aeac8fd1ce5fae60d89cb364f
Johannes Ferreira <johannesssf@gmail.com>
2020-07-10 15:57:06 -0300

    Add User model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField('e-mail')
+    password = models.CharField(
+        'password',
+        max_length=50,
+        validators=[MinLengthValidator(8)]
+    )
+
+    def __str__(self):
+        return self.email
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,39 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from model_bakery import baker
+
+from .models import User
+
+
+class TestUser(TestCase):
+
+    def setUp(self):
+        self.user = baker.make('api.User')
+        self.user.full_clean()
+
+    def test_user_creation(self):
+        self.assertEqual(self.user, User.objects.get(id=self.user.id))
+
+    def test_user_invalid_email(self):
+        with self.assertRaises(ValidationError):
+            user = User.objects.create(
+                email="invalid.email",
+                password="valid_password"
+            )
+            user.full_clean()
+
+        with self.assertRaises(ValidationError):
+            user = User.objects.create(
+                email="invalid@email",
+                password="valid_password"
+            )
+            user.full_clean()
+
+    def test_user_invalid_password(self):
+        with self.assertRaises(ValidationError):
+            user = User.objects.create(
+                email="valid@email",
+                password="passwd"
+            )
+            user.full_clean()
