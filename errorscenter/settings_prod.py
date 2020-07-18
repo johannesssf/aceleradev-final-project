5635ddeba207bbdac998fe2c730ab43efa1eb1e5
Johannes Ferreira <johannesssf@gmail.com>
2020-07-18 17:59:49 -0300

    Adjustments in production config

--- errorscenter/settings_prod.py
+++ errorscenter/settings_prod.py
@@ -8,9 +8,3 @@ DEBUG = False
 DATABASES = {
     'default': dj_database_url.config(),
 }
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
